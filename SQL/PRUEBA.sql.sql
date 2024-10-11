fdefdefcf357f5c4340cdec2a1cfc23c6139a32a
unaltradirezione <unaltradirezione@hotmail.com>
2024-10-11 14:27:23 -0600

    se agrega pruebas .sql

---
+++ SQL/PRUEBA.sql.sql
@@ -0,0 +1,21 @@
+
+------------------------------FFT------------------------------------
+
+SELECT *
+FROM based_deductibles
+WHERE
+    user_id = 5727 
+    and fft_id= 2957;
+
+SELECT *
+from crawling
+WHERE
+    user_id = '19887'
+    and "type" = 'TAX_COMPLIANCE';
+
+
+
+SELECT STATUS FROM FFT WHERE PERIOD = '2024-9';
+
+
+UPDATE fft SET STATUS = 'IN_PROGRESS' WHERE PERIOD = '2024-9'; 
