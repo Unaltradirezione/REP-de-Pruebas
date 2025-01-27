f5df31be30995cd1cc4b7e587821974b886e6b93
unaltradirezione <unaltradirezione@hotmail.com>
2025-01-27 07:45:27 -0600

    uso de extension de pruebas

---
+++ Extencion para comentarios/README.js
@@ -0,0 +1,10 @@
+// TODO: Implementar validación de entrada
+// FIXME: Corregir el error de división por cero
+// NOTE: Este bloque es temporal
+
+//todo
+
+Comentarios // ! en rojo.
+Comentarios // TODO en amarillo.
+Comentarios // * en verde.
+Comentarios // ? en azul.
